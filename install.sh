04118dc9c003e3a34adfaac1656bf44d262e6f0b
Joan Jené <joan.jene@cragenomica.es>
2017-06-02 15:46:08 +0200

    icons mac os x

--- install.sh
+++ install.sh
@@ -7,6 +7,16 @@ if [ "$(uname)" == "Darwin" ]; then
     cd development-environment
     docker build -t development-environment .
     cd ../..
+    # Take an image and make the image its own icon:
+    sips -i icons/ngasp_icon.png
+    # Extract the icon to its own resource file:
+    DeRez -only icns icons/ngasp_icon.png > icons/tmpicns.rsrc
+    # append this resource to the file you want to icon-ize.
+    Rez -append icons/tmpicns.rsrc -o ./runme.sh
+    # Use the resource to set the icon.
+    SetFile -a C ./runme.sh
+    # clean up.
+    rm icons/tmpicns.rsrc
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "GNU/Linux platform detected..."
     cd docker
@@ -14,6 +24,14 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     cd development-environment
     docker build -t development-environment .
     cd ../..
+    NGASP_PATH=`pwd`
+    echo "[Desktop Entry]" > ngasp.desktop
+    echo "Type=Application" >> ngasp.desktop
+    echo "Name=ngasp" >> ngasp.desktop
+    echo "Exec=/bin/bash -c 'cd \"\$(dirname %k)\" && $NGASP_PATH/runme.sh'" >> ngasp.desktop
+    echo "Icon=$NGASP_PATH/icons/ngasp_icon_black.png" >> ngasp.desktop
+    echo "Name[es_ES]=ngasp" >> ngasp.desktop
+    chmod +x ngasp.desktop 
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "32 bits Windows NT platform detected..."
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then

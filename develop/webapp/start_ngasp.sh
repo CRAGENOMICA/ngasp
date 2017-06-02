a3b04c79d6093b6d6065f22f80e0a7b1c2141a13
Joan Jené <joan.jene@cragenomica.es>
2017-06-02 12:49:17 +0200

    ngasp uses chrome instead of firefox, now + icon added. (Linux)

--- develop/webapp/start_ngasp.sh
+++ develop/webapp/start_ngasp.sh
@@ -29,6 +29,6 @@ echo
 echo "Opening http://localhost:3000..."
 echo
 systemd-machine-id-setup
-firefox http://localhost:3000
+google-chrome --no-sandbox --disable-bundled-ppapi-flash --app=http://localhost:3000
 # xdg-open
 
--- runme.sh
+++ runme.sh
@@ -17,7 +17,13 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo "GNU/Linux platform detected..."
     NGASP_PATH=`pwd`
     cd docker/development-environment
-    docker run -it --rm --privileged -p 3000:3000 -p 3001:3001 -e DISPLAY=$DISPLAY -v $NGASP_PATH/develop:/develop -v /tmp/.X11-unix:/tmp/.X11-unix development-environment /develop/webapp/start_ngasp.sh
+    # docker run -it --rm --privileged --net host --memory 512mb -p 3000:3000 -p 3001:3001 -e DISPLAY=$DISPLAY -v $NGASP_PATH/develop:/develop -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH development-environment /bin/bash
+    # gnome-terminal -e "XAUTH=`mktemp`;xauth nlist ${DISPLAY}| sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -;docker run -it --rm --privileged --net host -p 3000:3000 -p 3001:3001 -e DISPLAY=$DISPLAY -v $NGASP_PATH/develop:/develop -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH development-environment /develop/webapp/start_ngasp.sh"
+    # gnome-terminal -e "docker run -it --rm --privileged --net host -p 3000:3000 -p 3001:3001 -e DISPLAY=$DISPLAY -v $NGASP_PATH/develop:/develop -v /tmp/.X11-unix:/tmp/.X11-unix development-environment /develop/webapp/start_ngasp.sh"
+    XAUTH=`mktemp`
+    xauth nlist ${DISPLAY}| sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+    # The shm-size is for chrome. Without this option it crashes when changing the window size
+    gnome-terminal -e "docker run -it --rm --privileged --net host  --shm-size=1g --memory 1024mb -p 3000:3000 -p 3001:3001 -e DISPLAY=$DISPLAY -v $NGASP_PATH/develop:/develop -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH development-environment /develop/webapp/start_ngasp.sh"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "32 bits Windows NT platform detected..."
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then

e0c0a118ae6b1109dcbd9373abeb6b055e3b7270
Hgracia <ghgracia85@gmail.com>
2019-03-26 12:42:35 +0100

    Intento reducir mas el docker

--- src/compile_all.sh
+++ src/compile_all.sh
@@ -9,6 +9,92 @@
 
 cd /develop
 
+# ***********
+# *** gsl ***
+# ***********
+
+mkdir -p /tmp/gsl && \
+    curl -o /tmp/gsl-2.2.tar.gz ftp://ftp.gnu.org/gnu/gsl/gsl-2.2.tar.gz -LOk && \
+    tar -zxvf /tmp/gsl-2.2.tar.gz -C /tmp/gsl && \
+    rm /tmp/gsl-2.2.tar.gz && \
+    cd /tmp/gsl/gsl-2.2 && \
+    ./configure && \
+    make && \
+    make install && rm -rf /tmp/gs
+
+# ************
+# *** zlib ***
+# ************
+
+mkdir -p /tmp/zlib && \
+    curl -o  /tmp/zlib/zlib-1.2.10.tar.gz http://zlib.net/fossils/zlib-1.2.10.tar.gz -LOk && \
+    tar -zxvf /tmp/zlib/zlib-1.2.10.tar.gz -C /tmp/zlib && \
+    rm /tmp/zlib/zlib-1.2.10.tar.gz && \
+    cd /tmp/zlib/zlib-1.2.10 && \
+    ./configure && \
+    make && \
+    make install && rm -rf /tmp/zlib
+
+# **************
+# *** Htslib ***
+# **************
+
+mkdir -p /tmp/htslib && \
+    curl -o /tmp/htslib/htslib-1.3.1.tar.bz2 https://github.com/samtools/htslib/releases/download/1.3.1/htslib-1.3.1.tar.bz2 -LOk && \
+    tar jxf /tmp/htslib/htslib-1.3.1.tar.bz2 -C /tmp/htslib && \
+    rm /tmp/htslib/htslib-1.3.1.tar.bz2 && \
+    cd /tmp/htslib/htslib-1.3.1 && \
+    ./configure && \
+    make && \
+    make install && rm -rf /tmp/htslib
+
+# ***************
+# *** openmpi ***
+# ***************
+
+mkdir /tmp/openmpi && \
+    cd /tmp/openmpi && \
+    curl -o openmpi-1.10.1.tar.bz2 https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.1.tar.bz2 -LOk && \
+    tar -jxf openmpi-1.10.1.tar.bz2 && \
+    cd openmpi-1.10.1/ && \
+    ./configure --prefix=/opt/lib/openmpi && \
+    make all && \
+    make install && rm -rf /tmp/openmpi
+
+# ************
+# *** epel ***
+# ************
+
+curl -o epel.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -LOk && \
+    rpm -Uvh epel.rpm
+
+# ****************
+# *** Java JDK ***
+# ****************
+
+yum install -y http://vault.centos.org/7.3.1611/updates/x86_64/Packages/java-1.8.0-openjdk-1.8.0.131-3.b12.el7_3.x86_64.rpm http://vault.centos.org/7.3.1611/updates/x86_64/Packages/java-1.8.0-openjdk-headless-1.8.0.131-3.b12.el7_3.x86_64.rpm && yum install -y nodejs && yum clean all
+
+# **************
+# *** Gradle ***
+# **************
+
+mkdir -p /app/gradle && \
+    curl -o /app/gradle/gradle-3.1-bin.zip https://services.gradle.org/distributions/gradle-3.1-bin.zip -LOk && \
+    unzip -o /app/gradle/gradle-3.1-bin.zip -d /app/gradle && \
+    rm /app/gradle/gradle-3.1-bin.zip && \
+    echo "export PATH=\$PATH:/app/gradle/gradle-3.1/bin" > /etc/profile.d/gradle.sh && \
+    echo "export PATH=\$PATH:/app/gradle/gradle-3.1/bin" >> ~/.bashrc
+
+# *************
+# *** yaml *** 
+# *************
+
+mkdir -p /opt/lib/yaml && \
+    cd /opt/lib/yaml && \
+    wget https://github.com/jbeder/yaml-cpp/archive/master.zip && \
+    unzip -o master.zip && \
+    rm master.zip
+
 # *******************
 # *** fastaconvtr ***
 # *******************

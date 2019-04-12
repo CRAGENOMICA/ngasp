3a60810126025e182d1e9a1099d1d19d5ee891d4
Hgracia <ghgracia85@gmail.com>
2019-04-12 13:50:56 +0200

    añado examples mstatspop

--- CI.sh
+++ CI.sh
@@ -1,8 +1,9 @@
 #posible CI
 
-#TODO cambiar el nombre a dockerfile_compile
-docker build -f 'Dockerfile_1GB' -t ngasp-c .
+#TODO esto hay que pensarlo mas, porque ahora mismo es bastante engorroso
+
+docker build -f 'Dockerfile_develop' -t ngasp:develop .
 #este paso no esta muy bien pensado
-#una vez compilado todo, sacar laas librerias y los binarios y construir el docker lijero
-docker run --rm --net host -v `pwd`/src/librerias:/develop/librerias -v `pwd`/src/webapp/bin:/develop/webapp/bin --entrypoint /develop/librerias/extract_libraries.sh ngasp
-docker build -f Dockerfile -t ngasp .
+#una vez compilado todo, sacar las librerias y los binarios y construir el docker lijero
+docker run --rm --net host -v `pwd`/src/librerias:/develop/librerias -v `pwd`/src/webapp/bin:/develop/webapp/bin --entrypoint /develop/librerias/extract_libraries.sh ngasp:develop
+docker build -f 'Dockerfile_localManager' -t ngasp:localManager .
